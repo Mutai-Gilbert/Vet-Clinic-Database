c10ec9eed1b504407a6c7ae3bc3b4d85c1e9a4d7
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-03 11:13:06 +0300

    Added new files

--- queries.sql
+++ queries.sql
@@ -21,3 +21,36 @@ SELECT * FROM animals WHERE neutered = true;
 
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+// Inside a transaction update the animals table by setting the species column to unspecified. 
+//Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
+
+BEGIN TRANSACTION;
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+// Inside a transaction:
+//Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+//Update the animals table by setting the species column to pokemon for all animals that dont have species already set.
+//Commit the transaction.
+//Verify that change was made and persists after commit.
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+SELECT * FROM animals;
