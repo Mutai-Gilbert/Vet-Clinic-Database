af7bc276bbdaea078146d83d9433d0e6a4d3158a
Caleb Nwaizu <caleb.nwaizu@gurugeeksroyalty.biz>
2023-05-10 11:17:39 +0100

    Add indices to tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,6 +3,7 @@ CREATE TABLE patients (
 	name VARCHAR(120),
 	date_of_birth DATE
 );
+CREATE INDEX name_idx ON patients(name);
 
 CREATE TABLE medical_histories (
 	id SERIAL PRIMARY KEY,
@@ -11,6 +12,7 @@ CREATE TABLE medical_histories (
 	FOREIGN KEY (patient_id) REFERENCES patients(id),
 	status VARCHAR(255)
 );
+CREATE INDEX patient_id_idx ON medical_histories(patient_id);
 
 CREATE TABLE invoices (
 	id SERIAL PRIMARY KEY,
@@ -20,6 +22,7 @@ CREATE TABLE invoices (
 	medical_history_id INTEGER,
 	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
+CREATE INDEX medical_history_id_idx ON medical_histories(medical_history_id);
 
 CREATE TABLE invoices_items (
 	id SERIAL PRIMARY KEY,
@@ -30,12 +33,14 @@ CREATE TABLE invoices_items (
 	treatment_id INTEGER,
 	FOREIGN KEY (invoice_id) REFERENCES invoices(id)
 );
+CREATE INDEX invoice_id_idx ON invoices_items(invoice_id);
 
 CREATE TABLE treatments (
 	id SERIAL PRIMARY KEY,
 	type VARCHAR(255),
 	name VARCHAR(255)
 );
+CREATE INDEX name_idx ON treatments(id);
 
 ALTER TABLE invoices_items
 ADD CONSTRAINT fk_invoice_id
@@ -47,3 +52,4 @@ CREATE TABLE medical_treatment (
 	medical_histories_id INTEGER,
 	PRIMARY KEY (treatment_id, medical_histories_id)
 );
+CREATE INDEX medical_treatment_idx ON medical_treatment(treatment_id);
