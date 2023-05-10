f74c76452ad581704a33bac260d507fccae0e344
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-10 13:10:08 +0300

    Added new files to the schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,39 @@ CREATE TABLE medical_histories (
 	FOREIGN KEY (patient_id) REFERENCES patients(id),
 	status VARCHAR(255)
 );
+
+CREATE TABLE invoices (
+	id SERIAL PRIMARY KEY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INTEGER,
+	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoices_items (
+	id SERIAL PRIMARY KEY,
+	unit_price DECIMAL,
+	quantity INTEGER,
+	total_price DECIMAL,
+	invoice_id INTEGER,
+	treatment_id INTEGER,
+	FOREIGN KEY (invoice_id) REFERENCES invoices(id)
+);
+
+CREATE TABLE treatments (
+	id SERIAL PRIMARY KEY,
+	type VARCHAR(255),
+	name VARCHAR(255)
+);
+
+ALTER TABLE invoices_items
+ADD CONSTRAINT fk_invoice_id
+FOREIGN KEY (treatment_id)
+REFERENCES  treatments(id);
+
+CREATE TABLE medical_treatment (
+	treatment_id INTEGER,
+	medical_histories_id INTEGER,
+	PRIMARY KEY (treatment_id, medical_histories_id)
+);
